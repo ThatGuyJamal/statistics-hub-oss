a61edcc4df5c84475680c526ecaa2e513a8928b4
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-04-20 08:42:08 -0400

    +

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash    
 
-node -v
+# Making sure nodejs is installed on the server
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
+# Checking the version of nodejs installed
+node --version
+
+#  Installing and running the app
 npm install
 npm run compile
 npm run start
