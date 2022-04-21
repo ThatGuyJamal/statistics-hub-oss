24ac8d31d36436d86e71a67ed517d39ed50338ee
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-04-21 13:18:25 -0400

    Create query.test.ts

---
+++ backend/src/test/db/query.test.ts
@@ -0,0 +1,23 @@
+import {GuildDocumentModel} from "../../lib/database/guild/model" 
+
+const query = GuildDocumentModel
+
+query.findOneAndUpdate(
+    {_id: '123'},
+    {
+        $set: {
+            premium: {
+                paymentId: '123',
+                status: true,
+                tier: 1,
+                total_guilds_enabled: 1
+            }
+        }
+    },
+    {
+        upsert: true,
+        new: true
+    }
+).then((doc) => {
+    console.log(doc)
+})
